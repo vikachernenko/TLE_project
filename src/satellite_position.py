2a4ce3cbc4475d3f8c0b51e28839ce207b5da3c7
Федянин Владислав Станиславович <vladfed0675@gmail.com>
2025-05-20 20:13:17 +0300

    Добавлены основные функции расчёта положения спутника
    Добавлены функция расчета положения спутника в EarthMJ2000 и географических координатах, функция расчёта времени пролета, и функция определения положения спутника относительно наблюдателя

---
+++ src/satellite_position.py
@@ -0,0 +1,61 @@
+import numpy as np
+from pyorbital.orbital import Orbital
+from datetime import datetime, timedelta
+
+class Satellite:
+    def __init__(self, sat_name, tle1, tle2 ):
+        """
+        Инициализация спутника
+
+        :param sat_name: Название спутника
+        :param tle1: Первая строка TLE
+        :param tle2: Вторая строка TLE
+        """
+        self.name = sat_name
+        self.tle1 = tle1
+        self.tle2 = tle2
+        self.orb = Orbital(sat_name, line1=tle1, line2=tle2)
+
+    def calculate_satellite_position(self, timestamp):
+        """
+        Рассчитывает позицию спутника в системе EarthMJ2000 и географические координаты.
+        
+        :param timestamp: Временная метка (datetime в UTC)
+        :return: Словарь с координатами спутника
+        """
+
+        position, _ = self.orb.get_position(timestamp, normalize=False)
+        lon, lat, alt = self.orb.get_lonlatalt(timestamp)
+        return {
+            'earth_mj2000': position,
+            'latitude': lat,
+            'longitude': lon,
+            'altitude': alt
+        }
+    
+    def get_observer_look(self, ground_station, timestamp):
+        """
+        Рассчитывает позицию спутника в сферической системе координат относительно наблюдателя
+        
+        :param ground_station: Словарь с 'lat', 'lon', 'alt' станции
+        :param timestamp: Временная метка (datetime в UTC)
+        :return: Словарь с азимутом и возвышением
+        """
+
+        az,elev=self.orb.get_observer_look(timestamp, ground_station['lon'], ground_station['lat'], ground_station['alt'])
+        return{
+            'azimuth': az,
+            'elevation': elev 
+        }
+
+    def get_contacts_times(self, ground_station, timestamp, duration, horizon = 0.0):
+        """
+        Находит ближайшее время контакта спутника с наземной станцией.
+
+        :param ground_station: Словарь с 'lat', 'lon', 'alt' станции
+        :param timestamp: Начало периода поиска (datetime в UTC)
+        :param duration: В течении скольки часов искать контакт
+        :param horizon: Минимальный угол возвыщения (по умочанию 0.0)
+        :return: Массив кортежей (время начала контакта, время конца контакта, время максимального возвышения) (datetime в UTC)
+        """
+        return self.orb.get_next_passes(timestamp, duration, ground_station['lon'], ground_station['lat'], ground_station['alt'], horizon)
